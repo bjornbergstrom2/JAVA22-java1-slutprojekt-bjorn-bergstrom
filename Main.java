fec8a37297bf6ca6120fd5b6e808eafd98d424b9
Björn Bergström <112392165+bjornbergstrom2@users.noreply.github.com>
2022-10-28 18:18:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+        Swing swing = new Swing(today);
+        Date date = new Date(today);
+        CreateFile createFile = new CreateFile();
+        Scanner sc = new Scanner(System.in);
+        String file = "";
+
+        swing.createGUI();
+
+        System.out.println("\nPress ENTER to create a txt file.");
+        sc.nextLine();
+
+        // Sparar alla datum & strängar i [file] från kalendern
+        for (int i=0;i<swing.label.length;i++)
+            file += date.printDate(i) + "\n" + swing.text[i] + "\n\n";
+
+        createFile.fileCreater(file);
+
+    }
+}
